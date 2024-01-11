0026e17f7c3ce15a371e77b6f5388ef0c4a6035f
Gustavo Soares <gustavohvs.dev@gmail.com>
2024-01-11 10:29:56 -0300

    classes, generators and exceptions

---
+++ 2-Associate Module/8-Generators/2-ClassGeneretor.py
@@ -0,0 +1,28 @@
+#__iter__() which should return the object itself and which is invoked once (it's needed for Python to successfully start the iteration)
+#__next__() which is intended to return the next value (first, second, and so on) of the desired series - it will be invoked by the for/in statements in order to pass through the next iteration; if there are no more values to provide, the method should raise the StopIteration exception.
+
+class Fib:
+    def __init__(self, nn):
+        print("__init__")
+        self.__n = nn
+        self.__i = 0
+        self.__p1 = self.__p2 = 1
+
+    def __iter__(self):
+        print("__iter__")
+        return self
+
+    def __next__(self):
+        print("__next__")				
+        self.__i += 1
+        if self.__i > self.__n:
+            raise StopIteration
+        if self.__i in [1, 2]:
+            return 1
+        ret = self.__p1 + self.__p2
+        self.__p1, self.__p2 = self.__p2, ret
+        return ret
+
+
+for i in Fib(10):
+    print(i)
